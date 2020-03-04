382a7cd0afb2646122d840c90679f8fbef877f8b
SSep <ssep4u@gmail.com>
2020-03-04 11:08:02 +0900

    my_to_do_app/templates, views, urls createTodo

--- my_to_do_app/views.py
+++ my_to_do_app/views.py
@@ -1,5 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def index(request):
   return render(request, 'my_to_do_app/index.html')
+
+
+def createTodo(request):
+  return HttpResponse('create Todo를 할 거야!')
