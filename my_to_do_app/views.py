bc2c3d83776bd09f33e98f7fc1fbe7d5daadcc73
SSep <ssep4u@gmail.com>
2020-03-04 11:57:52 +0900

    my_to_do_app/templates, views, urls deleteTodo

--- my_to_do_app/views.py
+++ my_to_do_app/views.py
@@ -16,3 +16,11 @@ def createTodo(request):
   new_todo = Todo(content=user_input_str)
   new_todo.save()
   return HttpResponseRedirect(reverse('index'))
+
+
+def doneTodo(request):
+  done_todo_id = request.GET['todoNum']
+  print('완료한 todo의 id', done_todo_id)
+  todo = Todo.objects.get(id=done_todo_id)
+  todo.delete()
+  return HttpResponseRedirect(reverse('index'))
