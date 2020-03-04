d9c99aaaf0954d6f8d0adc33ee98066a8f9d2319
SSep <ssep4u@gmail.com>
2020-03-04 11:43:49 +0900

    my_to_do_app/templates, views, urls index

--- my_to_do_app/views.py
+++ my_to_do_app/views.py
@@ -1,15 +1,18 @@
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 from .models import Todo
 
 
 def index(request):
-  return render(request, 'my_to_do_app/index.html')
+  todos = Todo.objects.all()
+  content = {'todos': todos}
+  return render(request, 'my_to_do_app/index.html', content)
 
 
 def createTodo(request):
   user_input_str = request.POST['todoContent']
   new_todo = Todo(content=user_input_str)
   new_todo.save()
-  return HttpResponse('create Todo를 할 거야! =>' + user_input_str)
+  return HttpResponseRedirect(reverse('index'))
