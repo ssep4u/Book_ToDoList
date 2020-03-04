3211815717bc8f65d42b7f96d280db7651d9c183
SSep <ssep4u@gmail.com>
2020-03-04 11:30:19 +0900

    my_to_do_app/views save()

--- my_to_do_app/views.py
+++ my_to_do_app/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Todo
+
 
 def index(request):
   return render(request, 'my_to_do_app/index.html')
@@ -8,4 +10,6 @@ def index(request):
 
 def createTodo(request):
   user_input_str = request.POST['todoContent']
+  new_todo = Todo(content=user_input_str)
+  new_todo.save()
   return HttpResponse('create Todo를 할 거야! =>' + user_input_str)
